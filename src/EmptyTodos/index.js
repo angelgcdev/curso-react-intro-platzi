e9bdcd1b2a7ca9ca668b8227c3a3d18b1ec59a01
Luis Gutierrez <angelgcdev@gmail.com>
2024-12-24 07:28:18 -0400

    Reto: CreateTodoButton event & Modal CSS

---
+++ src/EmptyTodos/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./TodosLoading.css";
+
+function EmptyTodos() {
+  return <p>¡Crea tu primer TODO!...</p>;
+}
+
+export { EmptyTodos };
---
+++ src/TodosError/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+// import "./TodosLoading.css";
+
+function TodosError() {
+  return <p>Error...</p>;
+}
+
+export { TodosError };
